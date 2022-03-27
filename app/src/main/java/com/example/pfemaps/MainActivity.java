bde6bce7b2831284fb6918021f7469f7bd2c2ec9
alaedineibrahim <alaedineibrahim@gmail.com>
2022-03-27 13:04:15 +0100

    fixed markers

--- app/src/main/java/com/example/pfemaps/MainActivity.java
+++ app/src/main/java/com/example/pfemaps/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pfemaps;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -29,6 +31,9 @@ import android.view.MenuItem;
 
 import android.graphics.PointF;
 import android.view.View;
+import android.widget.Toast;
+
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.Projection;
 
@@ -43,10 +48,11 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.plugins.markerview.MarkerView;
 import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager;
-
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
-
+import com.mapbox.maps.plugin.annotation.Annotation;
+//import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions;
+//import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager;
 
 public class MainActivity extends AppCompatActivity {
     //private MapView mapView;
@@ -54,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     private MapView mapView;
     private MarkerView markerView;
     private MarkerViewManager markerViewManager;
+    DBHelper DB;
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -61,52 +68,131 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DB= new DBHelper(this,"pfeapp",1);
+/*
+        try{
+            DB.checkdb();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try {
+            DB.OpenDatabase();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+*/
+
+
+      /* Cursor mydata = DB.getdata();
+        while (mydata.moveToNext()){
+            System.out.println(mydata.getString(1));
+        }
+        */
+
        // Mapbox.get
        // Mapbox.getInstance(this, getString(R.string.mapbox_key));
        // setContentView(R.layout.activity_main);
         //mapView = (MapView) findViewById(R.id.mapView);
         //mapView.onCreate(savedInstanceState);
 
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        try {
+            SQLiteDatabase myDB = this.openOrCreateDatabase("pfeapp", MODE_PRIVATE, null);
 
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+            /* Create a Table in the Database. */
+            myDB.execSQL("CREATE TABLE IF NOT EXISTS "
+                    + "releve"
+                    + " (latx VARCHAR, langy VARCHAR);");
+
+            /* Insert data to a Table*/
+            myDB.execSQL("INSERT INTO "
+                    + "releve"
+                    + " (latx, langy)"
+                    + " VALUES ('33.892166', '9.561555499999997');");
+
+            /* Insert data to a Table*/
+            myDB.execSQL("INSERT INTO "
+                    + "releve"
+                    + " (latx, langy)"
+                    + " VALUES ('35.821430' , '10.634422');");
+
+            /* Insert data to a Table*/
+            myDB.execSQL("INSERT INTO "
+                    + "releve"
+                    + " (latx, langy)"
+                    + " VALUES ('34.73423', '10.761');");
+
+            /*retrieve data from database */
+            Cursor c = myDB.rawQuery("SELECT * FROM " + "releve" , null);
+
+            int Column1 = c.getColumnIndex("latx");
+            int Column2 = c.getColumnIndex("langy");
 
 
-        mapView.getMapAsync(new OnMapReadyCallback() {
 
-            @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
 
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        // Initialize the MarkerViewManager
-                        markerViewManager = new MarkerViewManager(mapView, mapboxMap);
+            binding = ActivityMainBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
 
-                        // Use an XML layout to create a View object
-                        View customView = LayoutInflater.from(MainActivity.this).inflate(
-                                R.layout.marker_view_bubble, null);
-                        customView.setLayoutParams(new FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
+            mapView = findViewById(R.id.mapView);
+            mapView.onCreate(savedInstanceState);
 
-                        ImageView imgExample = (ImageView) customView.findViewById(R.id.imageView);
+
+            mapView.getMapAsync(new OnMapReadyCallback() {
+
+                @Override
+                public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                    // Check if our result was valid.
+                    c.moveToFirst();
+                    if (c != null) {
+                        // Loop through all Results
+                        do {
+                            double latx = Double.parseDouble(c.getString(Column1));
+                            double langy = Double.parseDouble(c.getString(Column2));
+
+                            // One way to add a marker view
+                            mapboxMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(latx, langy))
+                                    .title("Chicago")
+                                    .snippet("Illinois")
+                            );
+
+
+                            // Toast.makeText(this,"latx:"+latx,Toast.LENGTH_SHORT).show();
+                        }while(c.moveToNext());
+                    }
+
+                    mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+
+                        @Override
+                        public void onStyleLoaded(@NonNull Style style) {
+                            // Initialize the MarkerViewManager
+                            //   markerViewManager = new MarkerViewManager(mapView, mapboxMap);
+
+
+                            // Use an XML layout to create a View object
+                            //View customView = LayoutInflater.from(MainActivity.this).inflate(
+                            //     R.layout.marker_view_bubble, null);
+                            //  customView.setLayoutParams(new FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
+
+                            //ImageView imgExample = (ImageView) customView.findViewById(R.id.imageView);
 
 
                             // Use the View to create a MarkerView which will eventually be given to
-    // the plugin's MarkerViewManager class
-                        markerView = new MarkerView(new LatLng(33.892166, 9.561555499999997), customView);
-                        markerViewManager.addMarker(markerView);
+                            // the plugin's MarkerViewManager class
+                            // markerView = new MarkerView(new LatLng(33.892166, 9.561555499999997), customView);
+                            //   markerViewManager.addMarker(markerView);
 
 
-
-                    }
-                });
-            }
-        });
-
+                        }
+                    });
+                }
+            });
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         //first initialize Mapbox
 
        // mapView.getMapboxMap().loadStyleUri(  Style.MAPBOX_STREETS   );
@@ -156,50 +242,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-/*
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (markerViewManager != null) {
-            markerViewManager.onDestroy();
-        }
-        mapView.onDestroy();
-    }
-*/
 }
